f818d0062d52717fd44327b3267c7ec49d932228
William Angelis <willangelis@gmail.com>
2022-01-11 14:03:36 -0300

    feat: :sparkles: Import tasklist and show task

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container,Main,Header } from './App.styles';
 import { useState } from 'react';
 import {Task} from './types/Task'
+import { Tasklist } from './components/Tasklist';
 
 const App = () => {
   const [tasks, setTasks] = useState<Task[]>([
@@ -11,7 +12,9 @@ const App = () => {
     <Container>
       <Main>
         <Header>Lista de Tarefas</Header>
-        
+        {tasks.map((item,index)=>(
+          <Tasklist key={index} item={item}/>
+        ))}
       </Main>
     </Container>
   );
