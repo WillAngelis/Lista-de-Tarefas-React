ed8ca984b6a99dea1dbe4d1612605d1ca6397316
William Angelis <willangelis@gmail.com>
2022-01-11 14:03:13 -0300

    feat: :sparkles: Add style for container for tasks

---
+++ src/components/Tasklist/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  background-color: #20212C;
+  padding: 10px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  align-items: center;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+
+  input{
+    width: 25px;
+    height: 25px;
+    margin-right: 15px;
+  }
+  
+  label{
+    color: #ccc;
+    font-weight: bold;
+  }
+`
