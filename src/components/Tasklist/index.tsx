051431b96a81fb8f8eab10552c68fcbc1230df57
William Angelis <willangelis@gmail.com>
2022-01-11 14:13:08 -0300

    feat: :sparkles: Add useState to change checkbox to true or false

--- src/components/Tasklist/index.tsx
+++ src/components/Tasklist/index.tsx
@@ -1,14 +1,21 @@
 import { Container } from './styles';
 import { Task } from '../../types/Task';
+import { useState } from 'react';
 
 type Props = {
   item: Task
 }
 
 export const Tasklist = ({item}: Props) =>{
+  const [isChecked, setIsChecked] = useState(item.checked);
+  
   return(
     <Container>
-      <input type="checkbox" />
+      <input 
+      type="checkbox" 
+      checked={isChecked}
+      onChange={ e => setIsChecked(e.target.checked)}
+      />
       <label>{item.task}</label>
     </Container>
   )
