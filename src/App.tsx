2ab2c7e5c57d2da74c32453cb3b46f3cf695cb1e
William Angelis <willangelis@gmail.com>
2022-01-13 15:15:47 -0300

    feat: :sparkles: Add useState to add new task

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ import { Tasklist } from './components/Tasklist';
 import { AddTaskBox } from './components/AddTaskBox';
 
 const App = () => {
-  const [tasks, setTasks] = useState<Task[]>([
-    {id : 1,task: 'Terminar Projeto',checked : false},
-  ]);
+  const [tasks, setTasks] = useState<Task[]>([]);
   
+  const handleAddTask = (taskName: string) =>{
+    let newTasks = [...tasks];
+      newTasks.push({
+      id: tasks.length + 1,
+      task : taskName,
+      checked : false
+    });
+    setTasks(newTasks)
+  }
   return(
     <Container>
       <Main>
         <Header>Lista de Tarefas</Header>
+        <AddTaskBox onEnter={handleAddTask}/>
 
           {tasks.map((item,index)=>(
         <Tasklist key={index} item={item}/>))}
