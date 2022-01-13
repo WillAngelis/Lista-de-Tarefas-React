b6972db236b4fdc920582a7486f521b46a06407b
William Angelis <willangelis@gmail.com>
2022-01-13 15:17:12 -0300

    feat: :sparkles: Add plus icon and import react icons

--- src/components/AddTaskBox/index.tsx
+++ src/components/AddTaskBox/index.tsx
@@ -1,9 +1,10 @@
 import { Container } from "./styles";
+import { IoIosAdd } from "react-icons/io";
 
 export const AddTaskBox = () =>{
   return(
     <Container>
-
+        <div className='add-task'> <IoIosAdd style={iconStyles}/> </div> 
     </Container>
   );
 }
